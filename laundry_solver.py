d6efd3a32ddc20bd329494eec578755847e43fd6
FrancoR <33163113+francoriboratig@users.noreply.github.com>
2021-09-22 21:41:31 -0300

    Añadidas primeras impresiones y programa dummy

---
+++ laundry_solver.py
@@ -0,0 +1,15 @@
+#Processes a file and transforms it in a model that can be processed by the program
+def process_file(file_name):
+    model = 0
+    print("This method does nothing right now")
+    return model
+
+#Processes the model generated and returns the solution it thinks it's better
+def evaluate_model(model):
+    result = 0
+    print("This method does nothing right now")
+    return result
+
+#Prints the solution to a .txt file with the propper 
+def output_file(result):
+    print("This method does nothing right now")
